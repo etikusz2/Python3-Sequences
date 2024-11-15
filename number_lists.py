7ff1dd4f209f7c34a6b417f41c6eb000548bf0ea
Birtalan Etele <etele.birtalan@gmail.com>
2024-11-15 19:43:56 +0200

    sorting function

--- number_lists.py
+++ number_lists.py
@@ -1,14 +1,15 @@
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
-print(min(even))
-print(max(even))
-print(min(odd))
-print(max(odd))
+even.extend(odd)
+print(even)
 
-print()
-print(len(even))
-print(len(odd))
+another_even = even
+print(another_even)
 
-print()
-print("mississippi".count("issi"))
+even.sort()
+print(even)
+
+even.sort(reverse=True)
+print(even)
+print(another_even)
