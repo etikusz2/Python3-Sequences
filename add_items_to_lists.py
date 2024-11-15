5731ddb415fafb3af94c908c0afed0b498059519
Birtalan Etele <etele.birtalan@gmail.com>
2024-11-15 19:30:23 +0200

    enumerate function and a code exercise (adding items into a list)

---
+++ add_items_to_lists.py
@@ -0,0 +1,32 @@
+# We have some data that contains a mixture of flowers and shrubs, in a list.
+# Our customer would like two separate lists.
+data = [
+    "Andromeda - Shrub",
+    "Bellflower - Flower",
+    "China Pink - Flower",
+    "Daffodil - Flower",
+    "Evening Primrose - Flower",
+    "French Marigold - Flower",
+    "Hydrangea - Shrub",
+    "Iris - Flower",
+    "Japanese Camellia - Shrub",
+    "Lavender - Shrub",
+    "Lilac- Shrub",
+    "Magnolia - Shrub",
+    "Peony - Shrub",
+    "Queen Anne's Lace - Flower",
+    "Red Hot Poker - Flower",
+    "Snapdragon - Flower",
+    "Sunflower - Flower",
+    "Tiger Lily - Flower",
+    "Witch Hazel - Shrub",
+]
+
+flowers = []
+shrubs = []
+
+for plant in data:
+    if "Flower" in plant:
+        flowers.append(plant)
+    else:
+        shrubs.append(plant)
