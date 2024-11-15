4a286b870e4f29acecf9656666519795584933c8
Birtalan Etele <etele.birtalan@gmail.com>
2024-11-15 18:40:29 +0200

    binding multiple names in a list

--- mutable.py
+++ mutable.py
@@ -7,3 +7,12 @@ print(id(another_list))
 shopping_list += ["cookies"]
 print(shopping_list)
 print(id(shopping_list))
+print(another_list)
+
+a = b = c = d = e = f = another_list
+print(a)
+
+print("Adding cream")
+b.append("cream")
+print(c)
+print(d)
