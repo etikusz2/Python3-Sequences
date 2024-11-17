56b69e243110c97f65dae6096333e26f2686c86d
Birtalan Etele <etele.birtalan@gmail.com>
2024-11-17 18:36:02 +0200

    the join method

--- nospam_challenge.py
+++ nospam_challenge.py
@@ -9,14 +9,8 @@ menu = [
     ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"]
 ]
 
-# for meal in menu:
-#     for index in range(len(meal) - 1, -1, -1):
-#         if meal[index] == "spam":
-#             del meal[index]
-#     print(meal)
-
 for meal in menu:
-    for item in meal:
-        if item != "spam":
-            print(item, end=" ")
-    print()
+    for index in range(len(meal) - 1, -1, -1):
+        if meal[index] == "spam":
+            del meal[index]
+    print(", ".join(meal))
