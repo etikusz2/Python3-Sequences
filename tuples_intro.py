86537510d2c248fcdf0714e50e59294cffdf236f
Birtalan Etele <etele.birtalan@gmail.com>
2024-11-19 18:36:24 +0200

    more unpacking

--- tuples_intro.py
+++ tuples_intro.py
@@ -4,15 +4,18 @@ budgie = "Nightflight" "Budgie", 1981
 imelda = "More Mayhem", "Emilda May", 2011
 metallica = "Ride the Lightning", "Metallica", 1984
 
-print(metallica)
-print(metallica[0])
-print(metallica[1])
-print(metallica[2])
+# print(metallica)
+# print(metallica[0])
+# print(metallica[1])
+# print(metallica[2])
 
-# metallica[0] = "Master of Puppets"
+title, artist, year = metallica
+print(title)
+print(artist)
+print(year)
 
-metallica2 = list(metallica)
-print(metallica2)
+table = ("Coffee table", 200, 100, 75, 34.50)
+print(table[1] * table[2])
 
-metallica2[0] = "Master of Puppets"
-print(metallica2)
+name, length, width, height, price = table
+print(length * width)
