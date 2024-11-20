b222fdfbaf29a971c1dcccf6921e26a1f4857707
Birtalan Etele <etele.birtalan@gmail.com>
2024-11-20 18:55:21 +0200

    simple jukebox

---
+++ jukebox_menu.py
@@ -0,0 +1,8 @@
+from nested_data import albums
+
+while True:
+    print("Please choose your album (invalid choice exits):")
+    for index, (title, artist, year, songs) in enumerate(albums):
+        print("{}: {}, {}, {}"
+              .format(index + 1, title, artist, year, songs))
+    break
--- nested_data.py
+++ nested_data.py
@@ -37,32 +37,3 @@ albums = [
      ]
      ),
 ]
-
-for name, artist, year, songs in albums:
-    print("Album: {}, Artist: {}, Year: {}, Songs: {}"
-          .format(name, artist, year, songs))
-
-print()
-
-album = albums[3]
-print(album)
-
-songs = album[3]
-print(songs)
-
-song = songs[2]
-print(song)
-print(song[1])
-
-mayhem = albums[3][3][2][1]
-print(mayhem)
-
-print(albums[3])
-print(albums[3][3])
-print(albums[3][3][2])
-print(albums[3][3][2][1])
-
-print(albums[1][3][5][1])
-print(albums[2][2])
-print(albums[3][3][3][0])
-print(albums[2][3][1])
