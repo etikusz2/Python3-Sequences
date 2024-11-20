2675a11ddf5a86da5a44ee1b621485e10065f099
Birtalan Etele <etele.birtalan@gmail.com>
2024-11-20 10:38:58 +0200

    nested tuples

--- tuples_intro.py
+++ tuples_intro.py
@@ -1,21 +1,13 @@
-welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
-bad = "Bad Company", "Bad Company", 1974
-budgie = "Nightflight" "Budgie", 1981
-imelda = "More Mayhem", "Emilda May", 2011
-metallica = "Ride the Lightning", "Metallica", 1984
+albums = [("Welcome to my Nightmare", "Alice Cooper", 1975),
+          ("Bad Company", "Bad Company", 1974),
+          ("Nightflight", "Budgie", 1981),
+          ("More Mayhem", "Emilda May", 2011),
+          ("Ride the Lightning", "Metallica", 1984),
+          ]
 
-# print(metallica)
-# print(metallica[0])
-# print(metallica[1])
-# print(metallica[2])
+print(len(albums))
 
-title, artist, year = metallica
-print(title)
-print(artist)
-print(year)
-
-table = ("Coffee table", 200, 100, 75, 34.50)
-print(table[1] * table[2])
-
-name, length, width, height, price = table
-print(length * width)
+for album in albums:
+    name, artist, year = album
+    print("Album: {}, Artist: {}, Year: {}"
+          .format(name, artist, year))
